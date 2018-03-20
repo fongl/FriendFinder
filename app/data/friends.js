00ae8a0171d97c36d2bddd2ad383f3b4a8906515
Lewis Fong <fongl@bu.edu>
2018-03-19 18:07:34 -0700

    added main files

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+
+var friendArray = [
+    {
+      name: "Greg",
+      photoUrl: "https://i.imgur.com/Yjqaez5.jpg",
+      question1: '5',
+      question2: '5',
+      question3: '5',
+      question4: '5',
+      question5: '5',
+      question6: '5',
+      question7: '5',
+      question8: '5',
+      question9: '5',
+      question10: '5'
+    },
+    {
+        name: "Kaylee",
+      photoUrl: "https://i.imgur.com/Yjqaez5.jpg",
+      question1: '1',
+      question2: '1',
+      question3: '1',
+      question4: '1',
+      question5: '1',
+      question6: '1',
+      question7: '1',
+      question8: '1',
+      question9: '1',
+      question10:'1'
+    }
+  ];
+ 
+  module.exports = friendArray;
+  
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+
+var friendData = require("../data/friends");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+      friendData.push(req.body);
+      console.log('thanks for submitting');
+      var comparer = [];
+      var minDiff = 50;
+      var match = 0;
+      for (var i = 1; i < 11 ; i++){
+        comparer.push(req.body['question'+i]);
+      }
+      for (var i = 0; i < friendData.length; i++)
+      {
+        var comparee = [];  
+        for (var j = 1; j <11; j++) {
+          comparee.push(friendData[i]['question'+j]);
+        }
+        var diff = 0;
+        for (var j = 0; j <comparer.length; j++) {
+          diff +=Math.abs(comparer[j]-comparee[j]);
+        }
+        if(diff<minDiff){
+          minDiff=diff;
+          match = i;
+        }
+      }
+      res.json(friendData[match]);
+  });
+
+};
